40bdc57eb7b64defa7002e9b41a527d2bb765837
meltintt <meltin.skosana@kingprice.co.za>
2026-09-18 18:13:53 +0200

    Fix import formatting in search_page.dart

--- lib/presentation/search/search_page.dart
+++ lib/presentation/search/search_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; 
 
 import '../../core/di.dart';
 import '../../data/repositories/books_repository.dart';
