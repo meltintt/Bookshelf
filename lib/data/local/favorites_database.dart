0f4122ffa2c77ae8f8d8681941d5104d695481a3
meltintt <meltin.skosana@kingprice.co.za>
2026-09-18 18:12:39 +0200

    Fix import formatting in favorites_database.dart

--- lib/data/local/favorites_database.dart
+++ lib/data/local/favorites_database.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:path/path.dart' as p;
+import 'package:path/path.dart' as p; 
 import 'package:sqflite/sqflite.dart';
 
 import '../domain/models/book.dart';
